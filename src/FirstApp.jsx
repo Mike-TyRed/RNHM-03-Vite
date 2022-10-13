610e8cce3687fc80f1c93190755f24339997e417
Miguel T. Rojo <miguel.terrazas193@tectijuana.edu.mx>
2022-10-13 10:50:56 -0700

    second

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,6 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//Fragmento
+//<> </>
+
+/* const newPost = () => {
+  return 12312 + 3123;
+}
+
+const newMessage = {
+  message: "Hello There",
+  title: "Title",
+} */
+
 //rafc
-export const FirstApp = () => {
+export const FirstApp = ({title, subTitle}) => {
+  //console.log(props);
   return (
-    <div>FirstApp</div>
+    <>
+      {/* <h1>{newPost()}</h1> renderizar un componente externo*/}
+      <h1>{title}</h1>
+      <p>{subTitle}</p>
+    </>
   )
+}
+
+FirstApp.propType = {
+  title: PropTypes.string.isRequired
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 //Llamada al React DOM para renderizar los componentes
 import {createRoot} from 'react-dom/client'
 import { FirstApp } from './FirstApp'
 import { HelloWorldApp } from './HelloWorldApp'
 
-function App(){
+//Importar CSS
+import './index.css'
+
+/* function App(){
     return <h1>Elden Ring</h1>
-}
+} */
 
 //Renderizar el componente
 const root = createRoot(document.getElementById('root'))
-root.render(<FirstApp/>)
+root.render(
+    <StrictMode>
+        <FirstApp/>
+    </StrictMode>
+);
